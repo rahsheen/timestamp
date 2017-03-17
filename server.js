ab60e6587065b030e1a3af7b073dd1ec7e6acd8e
R. Anthony Porter <rah@sheenonline.biz>
2017-03-17 12:40:49 -0400

    Completed server.
    Possible issue with returned unix timestamp. Still investigating.

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 var express = require("express")
+var app = express();
+
+app.get('/:date', function(req, res) {
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+    var data = req.params.date
+
+    var d = new Date(/[a-z]/i.test(data) ? data : +data)
+    var natDate = monthNames[d.getMonth()] +" "+ d.getDay()+", " + d.getFullYear()
+
+    var ret = {
+        "unix": d.getTime(),
+        "natural": natDate 
+    }
+
+    res.end(JSON.stringify(ret));
+})
+
+app.listen(8080)
